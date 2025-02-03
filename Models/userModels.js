3df16401a70ef25a1a4ae2618c4e5f5416690d1f
Priya Gupta <priyagupta052k@gmail.com>
2025-02-03 15:07:56 +0530

    Create Schema and Model for the data

--- Models/userModels.js
+++ Models/userModels.js
@@ -1,1 +1,33 @@
-// const {Schema , model } =require('mongoose');
+const {Schema , model } =require('mongoose');
+
+
+// creating schema
+// Schema deines the structure of data
+const userSchema = new Schema({
+    name:
+{
+    type:String,
+    required:true,
+    maxlength:50,
+},
+email:
+{
+    type:String,
+    required:true,
+},
+password:
+{
+type:String,
+required:true,
+minlength:8,
+maxlength:16,
+},
+});
+
+// creating Model using Schema
+// Model definesas an interface to interact with mongoDB
+const userModel = model('User',userSchema);
+
+
+// export the model
+module.exports = userModel
