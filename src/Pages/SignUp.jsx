f31865b1eb0519c8671ad6c89012f6454a32dbb6
Priya Gupta <priyagupta052k@gmail.com>
2025-02-12 22:57:54 +0530

    Update SignUp page

---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const SignUp = () =>{
+
+    const [name ,setName] = useState("");
+    const [reg , setReg] = useState("");
+    const [email,setEmail] = useState("");
+    const [phoneno , setPhoneno] =useState("");
+    const [password , setPassword] =useState("");
+
+handleSubmit()
+{
+
+}
+    return(
+        <div className="signup-container">
+            <div className="signup-box">
+                <h2>SignUp</h2>
+                <form onSubmit={handleSubmit}>
+                <label>Name</label><br/>
+                <input type="text" placeholder="Name" onChange={(e) =>setName(e.target.value)}/><br/>
+                <label>Register Number</label><br/>
+                <input type="text" placeholder="Register Number" onChange={(e) =>setReg(e.target.value)}/><br/>
+                <label>Phone number</label><br/>
+                <input type="number" placeholder="Phone number" onChange={(e) =>setPhoneno(e.target.value)}/><br/>
+                <label>Email id</label><br/>
+                <input type="email" placeholder="Email id"  onChange={(e) =>setEmail(e.target.value)}/><br/>
+                <label>Password</label><br/>
+                <input type="password" placeholder="Password" onChange={(e) =>setPassword(e.target.value)}/><br/>
+                <button type="button">Register</button>
+</form>
+<p>Already have a account ?</p>
+<Link to="/login">Login</Link>
+            </div>
+        </div>
+    )
+}
+
+export default SignUp;
